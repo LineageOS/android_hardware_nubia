BluedMC-Amethyst/hardware_xiaomi

interfaces: Split between vendors
This should make updates-makefiles.sh usable.

Change-Id: I118fcb1bcbed290eb894f92c058269189f2dde84

interfaces/updates-makefiles.sh
@@ -3,4 +3,7 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.xiaomi:hardware/xiaomi/interfaces"
+  "vendor.goodix:hardware/xiaomi/interfaces/goodix"
+
+do_makefiles_update \
+  "vendor.xiaomi:hardware/xiaomi/interfaces/xiaomi"

LineageOS/android_hardware_nubia

interfaces/updates-makefiles.sh
@@ -3,4 +3,4 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.nubia:hardware/nubia/interfaces"
+  "vendor.goodix:hardware/nubia/interfaces/goodix"